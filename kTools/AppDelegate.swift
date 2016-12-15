c547570a73f377ba384b459328b0103a1d9eacad
“Kiko <kiko@sunnyface.com>
2016-12-15 19:05:08 +0100

    Initial Commit

---
+++ kTools/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  kTools
+//
+//  Created by sMac on 15/12/2016.
+//  Copyright © 2016 Sunnyface.com. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
